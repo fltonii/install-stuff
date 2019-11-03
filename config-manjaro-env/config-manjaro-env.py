fffdcfc9e0e3e0baafeb77d8c5106404a96af58f
fltonii <tonialjoao97@gmail.com>
2019-11-03 20:20:48 -0300

    testing python file open

--- config-manjaro-env/config-manjaro-env.py
+++ config-manjaro-env/config-manjaro-env.py
@@ -1,5 +1,5 @@
 import os
 
-ext = open("./vscode-extensions.txt", "r").readlines()
+ext = open("https://raw.githubusercontent.com/fltonii/scripts/master/config-manjaro-env/vscode-extensions.txt", "r").readlines()
 for it in ext:
   os.system(f"code --install-extension {it}")
