6164d59eb0131d70f3e44fa75dedc862b7752195
fltonii <tonialjoao97@gmail.com>
2019-12-01 11:25:13 -0300

    removing temp file

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -Sy --noconfirm python3
 mkdir temp1
-cd temp1
-python ../install-from-csv.py
-cd -1 && rm -rf temp1
+{ 
+	cd temp1
+	python ../install-from-csv.py
+	cd -1 && rm -rf temp1
+} || rm -rf temp1
