26f208fb73e44a784435501a128dd422d02f8324
fltonii <tonialjoao97@gmail.com>
2019-12-01 11:35:45 -0300

    no temp file for now

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 sudo pacman -Syu --noconfirm
 sudo pacman -Sy --noconfirm python3
-mkdir temp1
-{ 
-	cd temp1
-	python ../install-from-csv.py
-	cd .. && rm -rf temp1
-} || rm -rf temp1
+python ../install-from-csv.py
