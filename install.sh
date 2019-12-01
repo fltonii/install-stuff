e504955ac5825d69e6d7c6d5df991e079862547c
fltonii <tonialjoao97@gmail.com>
2019-12-01 19:03:57 -0300

    adding yay to pre script step

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 sudo pacman -Syu --noconfirm
 
 if pacman -Qs python3 > /dev/null ; then
-  echo "python3 already installed"
+  	echo "=== python3 already installed ==="
 else
-  sudo pacman -Sy python3
+  	sudo pacman -Sy --noconfirm python3
 fi
+
+if pacman -Qs yay > /dev/null ; then
+	echo "=== yay already installed ==="
+else
+	sudo pacman -Sy --noconfirm yay
+fi
+
 python ./install-from-csv.py
