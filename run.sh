ab4a21b3f407bf3ede13a36ed3641953db813349
fltonii <tonialjoao97@gmail.com>
2019-12-08 14:04:37 -0300

    finishing up shell file

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+DIR="$(dirname "$(which "$0")")"
 
 readonly VSCODE_CONFIG=~/.config/Code/User
+readonly VSCODE_DIR=$DIR/vscode
+readonly VSCODE_EXTENSIONS=$VSCODE_DIR/extensions.txt
 
 #install prerequisites
 sudo pacman -Syu --noconfirm
@@ -14,6 +17,12 @@ git config --global user.name "fltonii"
 git config --global user.email "tonialjoao97@gmail.com"
 
 # setup vscode
-python vscode/install-vscode-extensions.py
-cp vscode/settings.json $VSCODE_CONFIG 
-cp vscode/keybindings.json $VSCODE_CONFIG
+cp $VSCODE_DIR/settings.json $VSCODE_CONFIG 
+cp $VSCODE_DIR/keybindings.json $VSCODE_CONFIG
+
+# install vscode extensions 
+
+echo $PATH
+while read extension; do
+	code --install-extension $extension
+done < $VSCODE_EXTENSIONS
