0fb71712ddf205ab5210fb878d58a2e68c624e3e
fltonii <tonialjoao97@gmail.com>
2019-12-07 23:36:26 -0300

    adding setup script

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -2,4 +2,4 @@ curl -s ./script.py https://raw.githubusercontent.com/fltonii/scripts/master/vsc
 cd ~/.config/Code/User
 curl https://raw.githubusercontent.com/fltonii/scripts/master/vscode/settings.json --output settings.json
 curl https://raw.githubusercontent.com/fltonii/scripts/master/vscode/keybindings.json --output keybindings.json
-cd -1
+cd ..
